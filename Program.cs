06df3258fb9e4f70a5c85b1d285579e5fe29cb57
Harris Brakmic <brakmic@gmail.com>
2023-10-08 15:42:36 +0200

    feat: add planner support

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Orchestration;
+using Microsoft.SemanticKernel.Planning;
 
 class Program
 {
@@ -31,7 +32,8 @@ class Program
         };
 
     rootCommand.SetHandler(
-        Run,
+        //Run,
+        RunWithPlanner,
         fileOption, functionOption
     );
 
@@ -71,6 +73,43 @@ class Program
 
       var result = await kernel.RunAsync(context, skillImport[function]);
       Console.WriteLine(result);
+    }
+  }
+
+  private static async Task RunWithPlanner(FileInfo file, string function)
+  {
+    var kernelSettings = KernelSettings.LoadSettings();
+
+    using ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
+    {
+      builder
+              .SetMinimumLevel(kernelSettings.LogLevel ?? LogLevel.Warning)
+              .AddConsole()
+              .AddDebug();
+    });
+
+    IKernel kernel = new KernelBuilder()
+        .WithLoggerFactory(loggerFactory)
+        .WithCompletionService(kernelSettings)
+        .Build();
+
+    if (kernelSettings.EndpointType == EndpointTypes.TextCompletion)
+    {
+      var rootDirectory = Configuration!.GetSection("SkillSettings:Root").Get<string>();
+      var pluginDirectories = Configuration.GetSection("SkillSettings:Plugins").Get<string[]>();
+
+      var skillsRoot = Path.Combine(Directory.GetCurrentDirectory(), rootDirectory!);
+      var skillImport = kernel.ImportSemanticSkillFromDirectory(skillsRoot, pluginDirectories!);
+      var keyGenPlugin = kernel.ImportSkill(new Plugins.KeyAndCertGenerator(), "KeyAndCertGeneratorPlugin");
+      var secretsPlugin = kernel.ImportSkill(new Plugins.Updater(), "SecretsUpdaterPlugin");
+
+      var planner = new SequentialPlanner(kernel);
+      var ask = await File.ReadAllTextAsync(file.FullName);
+      var plan = await planner.CreatePlanAsync(ask);
+
+      var result = await plan.InvokeAsync();
+
+      Console.WriteLine($"\nRESULT: {result}");
     }
   }
 }
