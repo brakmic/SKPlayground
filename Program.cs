237c1e4ca68bb9225bd2b7a2e90dd7e4ec778716
Harris Brakmic <brakmic@gmail.com>
2023-10-09 14:34:02 +0200

    feat: update demo code

--- Program.cs
+++ Program.cs
@@ -35,7 +35,8 @@ class Program
 
     rootCommand.SetHandler(
         //Run,
-        RunWithPlanner,
+        RunWithActionPlanner,
+        //RunWithSequentialPlanner,
         fileOption, functionOption
     );
 
@@ -77,8 +78,47 @@ class Program
       Console.WriteLine(result);
     }
   }
+  private static async Task RunWithActionPlanner(FileInfo file, string function)
+  {
+    var kernelSettings = KernelSettings.LoadSettings();
 
-  private static async Task RunWithPlanner(FileInfo file, string function)
+    using ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
+    {
+      builder
+              .SetMinimumLevel(kernelSettings.LogLevel ?? LogLevel.Warning)
+              .AddConsole()
+              .AddDebug();
+    });
+
+    IKernel kernel = new KernelBuilder()
+        .WithLoggerFactory(loggerFactory)
+        .WithCompletionService(kernelSettings)
+        .Build();
+
+    if (kernelSettings.EndpointType == EndpointTypes.TextCompletion)
+    {
+
+      var rootDirectory = Configuration!.GetSection("SkillSettings:Root").Get<string>();
+      var pluginDirectories = Configuration.GetSection("SkillSettings:Plugins").Get<string[]>();
+
+      var skillsRoot = Path.Combine(Directory.GetCurrentDirectory(), rootDirectory!);
+      var skillImport = kernel.ImportSemanticSkillFromDirectory(skillsRoot, pluginDirectories!);
+
+      var httpPlugin = kernel.ImportSkill(new HttpPlugin(), nameof(HttpPlugin));
+
+      var planner = new ActionPlanner(kernel);
+      var ask = await File.ReadAllTextAsync(file.FullName);
+      var plan = await planner.CreatePlanAsync(ask);
+
+      Console.WriteLine($"\nPLAN:\n{plan.ToSafePlanString()}");
+
+      var result = await plan.InvokeAsync();
+
+      Console.WriteLine($"\nRESULT: {result}");
+    }
+  }
+
+  private static async Task RunWithSequentialPlanner(FileInfo file, string function)
   {
     var kernelSettings = KernelSettings.LoadSettings();
 
