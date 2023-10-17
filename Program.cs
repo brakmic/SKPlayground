95dee4db3e277afde241f0ac494c62cf29aa66ed
brakmic-extern <92522482+brakmic-extern@users.noreply.github.com>
2023-10-17 11:54:58 +0200

    feat: add rag example code

--- Program.cs
+++ Program.cs
@@ -11,6 +11,17 @@ using SkPlayground.Plugins;
 using static Microsoft.SemanticKernel.SemanticFunctions.PromptTemplateConfig;
 using SkPlayground.Extensions;
 using Microsoft.SemanticKernel.Planners;
+using Microsoft.SemanticKernel.Connectors.Memory.Weaviate;
+using Microsoft.SemanticKernel.Connectors.Memory.Qdrant;
+using Microsoft.SemanticKernel.Connectors.Memory.Sqlite;
+using Microsoft.SemanticKernel.Plugins.Memory;
+using Microsoft.SemanticKernel.Connectors.AI.OpenAI.TextEmbedding;
+using Microsoft.SemanticKernel.TemplateEngine.Basic;
+using Microsoft.SemanticKernel.Memory;
+using SkPlayground.Models;
+using Microsoft.SemanticKernel.Connectors.AI.OpenAI;
+using Microsoft.SemanticKernel.Connectors.AI.OpenAI.ChatCompletion;
+using SkPlayground.Util.Helpers;
 
 namespace SkPlayground;
 class Program
@@ -40,11 +51,12 @@ class Program
         };
 
     rootCommand.SetHandler(
-        Run,
+        //Run,
         //RunWithActionPlanner,
         //RunWithSequentialPlanner,
         //RunWithHooks, /* this example uses the native function "ExecuteGet" from HttpPlugin */
         //RunWithHooks2, /* this example uses a semantic function and the Markdown converter function */
+        RunWithRag,
         fileOption, functionOption
     );
 
@@ -70,11 +82,11 @@ class Program
 
     if (kernelSettings.EndpointType == EndpointTypes.TextCompletion)
     {
-      var rootDirectory = Configuration!.GetSection("SkillSettings:Root").Get<string>();
-      var pluginDirectories = Configuration.GetSection("SkillSettings:Plugins").Get<string[]>();
+      var rootDirectory = Configuration!.GetSection("PluginSettings:Root").Get<string>();
+      var pluginDirectories = Configuration.GetSection("PluginSettings:Plugins").Get<string[]>();
 
-      var skillsRoot = Path.Combine(Directory.GetCurrentDirectory(), rootDirectory!);
-      var skillImport = kernel.ImportSemanticFunctionsFromDirectory(skillsRoot, pluginDirectories!);
+      var pluginsRoot = Path.Combine(Directory.GetCurrentDirectory(), rootDirectory!);
+      var pluginImport = kernel.ImportSemanticFunctionsFromDirectory(pluginsRoot, pluginDirectories!);
 
       string description = await File.ReadAllTextAsync(file.FullName);
       var context = new ContextVariables();
@@ -82,7 +94,7 @@ class Program
       string key = "input";
       context.Set(key, description);
 
-      var result = await kernel.RunAsync(context, skillImport[function]);
+      var result = await kernel.RunAsync(context, pluginImport[function]);
       Console.WriteLine(result.GetValue<string>());
     }
   }
@@ -106,11 +118,11 @@ class Program
     if (kernelSettings.EndpointType == EndpointTypes.TextCompletion)
     {
 
-      var rootDirectory = Configuration!.GetSection("SkillSettings:Root").Get<string>();
-      var pluginDirectories = Configuration.GetSection("SkillSettings:Plugins").Get<string[]>();
+      var rootDirectory = Configuration!.GetSection("PluginSettings:Root").Get<string>();
+      var pluginDirectories = Configuration.GetSection("PluginSettings:Plugins").Get<string[]>();
 
-      var skillsRoot = Path.Combine(Directory.GetCurrentDirectory(), rootDirectory!);
-      var skillImport = kernel.ImportSemanticFunctionsFromDirectory(skillsRoot, pluginDirectories!);
+      var pluginsRoot = Path.Combine(Directory.GetCurrentDirectory(), rootDirectory!);
+      var pluginImport = kernel.ImportSemanticFunctionsFromDirectory(pluginsRoot, pluginDirectories!);
 
       var httpPlugin = kernel.ImportFunctions(new HttpPlugin(), nameof(HttpPlugin));
 
@@ -125,7 +137,6 @@ class Program
       Console.WriteLine($"\nRESULT: {result.GetValue<string>()}");
     }
   }
-
   private static async Task RunWithSequentialPlanner(FileInfo file, string function)
   {
     var kernelSettings = KernelSettings.LoadSettings();
@@ -145,11 +156,11 @@ class Program
 
     if (kernelSettings.EndpointType == EndpointTypes.TextCompletion)
     {
-      var rootDirectory = Configuration!.GetSection("SkillSettings:Root").Get<string>();
-      var pluginDirectories = Configuration.GetSection("SkillSettings:Plugins").Get<string[]>();
+      var rootDirectory = Configuration!.GetSection("PluginSettings:Root").Get<string>();
+      var pluginDirectories = Configuration.GetSection("PluginSettings:Plugins").Get<string[]>();
 
-      var skillsRoot = Path.Combine(Directory.GetCurrentDirectory(), rootDirectory!);
-      var skillImport = kernel.ImportSemanticFunctionsFromDirectory(skillsRoot, pluginDirectories!);
+      var pluginsRoot = Path.Combine(Directory.GetCurrentDirectory(), rootDirectory!);
+      var pluginImport = kernel.ImportSemanticFunctionsFromDirectory(pluginsRoot, pluginDirectories!);
       var keyGenPlugin = kernel.ImportFunctions(new KeyAndCertGenerator(), nameof(KeyAndCertGenerator));
       var secretsPlugin = kernel.ImportFunctions(new SecretYamlUpdater(), nameof(SecretYamlUpdater));
 
@@ -164,7 +175,6 @@ class Program
       Console.WriteLine($"\nRESULT: {result.GetValue<string>()}");
     }
   }
-
   private static async Task RunWithHooks(FileInfo file, string function)
   {
     #region Kernel Setup
@@ -203,7 +213,6 @@ class Program
       Console.WriteLine($"\nRESULT: {result.GetValue<string>()}");
     }
   }
-
   private static async Task RunWithHooks2(FileInfo file, string function)
   {
     #region Kernel Setup
@@ -265,9 +274,179 @@ class Program
       var result = await kernel.RunAsync("Denis Ritchie", askAbutPerson);
     }
   }
+  private static async Task RunWithRag(FileInfo file, string function)
+  {
+    var kernelSettings = KernelSettings.LoadSettings();
+
+    using ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
+    {
+      builder
+              .SetMinimumLevel(kernelSettings.LogLevel ?? LogLevel.Warning)
+              .AddConsole()
+              .AddDebug();
+    });
+
+    IKernel kernel = Kernel.Builder
+                     .WithLoggerFactory(loggerFactory)
+                     .WithOpenAITextCompletionService(
+                            modelId: kernelSettings.DeploymentOrModelId,
+                            apiKey: kernelSettings.ApiKey,
+                            orgId: kernelSettings.OrgId,
+                            serviceId: kernelSettings.ServiceId)
+                    .WithOpenAITextEmbeddingGenerationService(
+                      modelId: "text-embedding-ada-002",
+                      apiKey: kernelSettings.ApiKey,
+                      orgId: kernelSettings.OrgId,
+                      serviceId: kernelSettings.ServiceId
+                    )
+                    .Build();
+
+    var rootDirectory = Configuration!.GetSection("PluginSettings:Root").Get<string>();
+    var pluginDirectories = Configuration.GetSection("PluginSettings:Plugins").Get<string[]>();
+
+    var pluginsRoot = Path.Combine(Directory.GetCurrentDirectory(), rootDirectory!);
+    var pluginImport = kernel.ImportSemanticFunctionsFromDirectory(pluginsRoot, pluginDirectories!);
+
+    //var memoryStore = new WeaviateMemoryStore(endpoint: "http://localhost:8080/v1", loggerFactory: loggerFactory);
+    //var memoryStore = new VolatileMemoryStore();
+    //var memoryStore = new QdrantMemoryStore("http://localhost:6333", 1536, loggerFactory: loggerFactory);
+    var memoryStore = await SqliteMemoryStore.ConnectAsync("memories.sqlite");
+
+    // Create an embedding generator to use for semantic memory.
+    var embeddingGenerator = new OpenAITextEmbeddingGeneration(modelId: "text-embedding-ada-002", apiKey: kernelSettings.ApiKey,
+                                                                organization: kernelSettings.OrgId,
+                                                                loggerFactory: loggerFactory);
+
+    // The combination of the text embedding generator and the memory store makes up the 'SemanticTextMemory' object used to
+    // store and retrieve memories.
+    var textMemory = new MemoryBuilder()
+                .WithLoggerFactory(loggerFactory)
+                .WithTextEmbeddingGeneration(embeddingGenerator)
+                .WithMemoryStore(memoryStore)
+                .Build();
+
+    // Alternatively, one could use SemanticTextMemory instance instead
+    //SemanticTextMemory textMemory = new(memoryStore, embeddingGenerator);
+
+    var memoryPlugin = new TextMemoryPlugin(textMemory);
+    var memoryFunctions = kernel.ImportFunctions(memoryPlugin, "MemoryPlugin");
+
+    var collection = "aboutme";
+
+    // use this to wipe memory
+    //await memoryStore.DeleteCollectionAsync(collection); return;
+
+    bool exists = await memoryStore.DoesCollectionExistAsync(collection);
+
+    if (!exists)
+    {
+      // there are two ways to populate the memory:
+      // * with Kernel + Plugins
+      await PopulateInterestingFacts(kernel, memoryFunctions, collection, FactHelper.GetFacts());
+      // * with SemanticTextMemory
+      //await PopulateInterestingFacts(textMemory, collection, FactHelper.GetFacts());
+    }
+
+    // * Three ways to use the memory
+
+    // * 1. By using TextMemory methods
+    //MemoryQueryResult? lookup = await textMemory.GetAsync(collection, "INSERT_ID_HERE");
+    //IAsyncEnumerable<MemoryQueryResult> lookup = textMemory.SearchAsync(collection, "Do I have pets?", 1, minRelevanceScore: 0.50);
+
+    // await foreach (var r in lookup)
+    // {
+    //   Console.WriteLine("Memory with key 'id': " + r.Metadata.Text ?? "ERROR: memory not found");
+    // }
+    //return;
+
+    // * 2. By using Kernel and TextMemoryPlugin
+    // var result = await kernel.RunAsync(memoryFunctions["Recall"], new()
+    // {
+    //   [TextMemoryPlugin.CollectionParam] = collection,
+    //   [TextMemoryPlugin.LimitParam] = "1",
+    //   [TextMemoryPlugin.RelevanceParam] = "0.79",
+    //   ["input"] = "Which season do I prefer?"
+    // });
+    // Console.WriteLine($"Answer: {result.GetValue<string>()}");
+
+    // * 3. By using the RAG Pattern (Retrieval-augmented Generation)
+    var answer = await RunMiniRAG(textMemory, kernel, collection, "Tell me something about my hobbies.");
+    Console.WriteLine(answer);
+
+  }
+
+  private static async Task<string> RunMiniRAG(ISemanticTextMemory memory, IKernel kernel, string collectionName, string input)
+  {
+    var context = kernel.CreateNewContext();
+    context.Variables.Add("user_input", input);
+
+    // retrieve user-specific context based on the user input
+    var searchResults = memory.SearchAsync(collectionName, input);
+    var retrieved = new List<string>();
+    await foreach (var item in searchResults)
+    {
+      retrieved.Add(item.Metadata.Text);
+    }
+    context.Variables.Add("user_context", string.Join(',', retrieved));
+
+    // run SK function and give it the two variables, input and context
+    var func = kernel.Functions.GetFunction("Assistant", "Chat");
+    var answer = await kernel.RunAsync(func, context.Variables);
+
+    return answer.GetValue<string>()!;
+
+  }
 
   /// <summary>
-  /// A helper function that converts plain text to markdown
+  /// Populates the memory with facts about an entity
+  /// </summary>
+  /// <param name="kernel">Kernel</param>
+  /// <param name="memoryFunctions">Memory Plugin</param>
+  /// <param name="collection">Collection name</param>
+  /// <param name="facts">List of facts</param>
+  /// <returns>Enumerable containing KernelResults</returns>
+  private static async Task<IEnumerable<KernelResult?>> PopulateInterestingFacts(IKernel kernel, IDictionary<string, ISKFunction> memoryFunctions, string collection, IEnumerable<Fact> facts)
+  {
+    var results = new List<KernelResult?>();
+    foreach (var fact in facts)
+    {
+      var result = await kernel.RunAsync(memoryFunctions["Save"], new()
+      {
+        [TextMemoryPlugin.CollectionParam] = collection,
+        [TextMemoryPlugin.KeyParam] = fact.Id,
+        ["input"] = fact.Text
+      });
+      results.Add(result);
+    }
+    return results;
+  }
+
+  /// <summary>
+  /// Populates the memory with facts about an entity
+  /// </summary>
+  /// <param name="memory">Memory</param>
+  /// <param name="collection">Collection name</param>
+  /// <param name="facts">List of facts</param>
+  /// <returns></returns>Enumerable containing IDs of saved memory records<summary>
+  private static async Task<IEnumerable<string>> PopulateInterestingFacts(ISemanticTextMemory memory, string collection, IEnumerable<Fact> facts)
+  {
+    var ids = new List<string>();
+    // Iterate through the facts and populate the memory
+    foreach (var fact in facts)
+    {
+      var id = await memory.SaveInformationAsync(
+        collection: collection,
+        text: fact.Text,
+        id: fact.Id,
+        description: fact.Description,
+        additionalMetadata: fact.AdditionalMetadata);
+      ids.Add(id);
+    }
+    return ids;
+  }
+
+  /// <summary>
+  /// Convert plain text to markdown
   /// </summary>
   /// <param name="plainText">Plain text string</param>
   /// <returns>Content as Markdown string</returns>
