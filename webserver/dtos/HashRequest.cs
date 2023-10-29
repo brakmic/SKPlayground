b46d2245652bb46b381186fa83697620903e1460
Harris Brakmic <brakmic@gmail.com>
2023-10-29 18:49:46 +0100

    feat: update dto hash-request

---
+++ webserver/dtos/HashRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SkPlayground.WebServer.Dtos;
+
+public class HashRequest
+{
+    [JsonPropertyName("input")]
+    public required string Input { get; set; }
+}
+
