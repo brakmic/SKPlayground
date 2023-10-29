68d5652712e54b1f294b87b2879e7f24e12a316c
Harris Brakmic <brakmic@gmail.com>
2023-10-29 18:50:09 +0100

    feat: add formatters

---
+++ webserver/formatters/HashRequestFormatter.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using SkPlayground.WebServer.Dtos;
+
+namespace SkPlayground.WebServer.Formatters;
+
+public class HashRequestFormatter : TextInputFormatter
+{
+    public HashRequestFormatter()
+    {
+        SupportedMediaTypes.Add("application/json");
+        SupportedMediaTypes.Add("text/plain");
+        SupportedEncodings.Add(System.Text.Encoding.UTF8);
+    }
+
+    protected override bool CanReadType(Type type)
+    {
+        if (type == typeof(HashRequestFormatter))
+        {
+            return base.CanReadType(type);
+        }
+        return false;
+    }
+
+    public override async Task<InputFormatterResult> ReadRequestBodyAsync(InputFormatterContext context, System.Text.Encoding encoding)
+    {
+        if (context == null)
+            throw new ArgumentNullException(nameof(context));
+
+        if (encoding == null)
+            throw new ArgumentNullException(nameof(encoding));
+
+        using var reader = new StreamReader(context.HttpContext.Request.Body, encoding);
+        try
+        {
+            var content = await reader.ReadToEndAsync();
+            var obj = JsonSerializer.Deserialize<HashRequestFormatter>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            return await InputFormatterResult.SuccessAsync(obj);
+        }
+        catch
+        {
+            return await InputFormatterResult.FailureAsync();
+        }
+    }
+}
---
+++ webserver/formatters/TextPlainInputFormatter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Formatters;
+using System.Text;
+
+namespace SkPlayground.WebServer.Formatters;
+
+public class TextPlainInputFormatter : TextInputFormatter
+{
+    public TextPlainInputFormatter()
+    {
+        SupportedMediaTypes.Add("text/plain");
+        SupportedEncodings.Add(UTF8EncodingWithoutBOM);
+        SupportedEncodings.Add(UTF16EncodingLittleEndian);
+    }
+
+    public override async Task<InputFormatterResult> ReadRequestBodyAsync(InputFormatterContext context, Encoding encoding)
+    {
+        using var reader = new StreamReader(context.HttpContext.Request.Body, encoding);
+        var content = await reader.ReadToEndAsync();
+        return await InputFormatterResult.SuccessAsync(content);
+    }
+}
