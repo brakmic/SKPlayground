4a276b6c5aa3f2fab3c97679f61ad393e9e5e991
Harris Brakmic <brakmic@gmail.com>
2023-10-29 18:49:59 +0100

    feat: add filters

---
+++ webserver/filters/JsonContentTypeOperationFilter.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace SkPlayground.WebServer.Filters;
+
+public class JsonContentTypeOperationFilter : IOperationFilter
+{
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
+    {
+        if (operation.RequestBody != null)
+        {
+            var content = operation.RequestBody.Content;
+            if (content.ContainsKey("text/plain"))
+            {
+               content.Remove("text/plain");
+            }
+            if (!content.ContainsKey("application/json"))
+            {
+                content.Add("application/json", new OpenApiMediaType());
+            }
+        }
+
+        foreach (var response in operation.Responses.Values)
+        {
+            var content = response.Content;
+            if (content.ContainsKey("text/plain"))
+            {
+               content.Remove("text/plain");
+            }
+            if (!content.ContainsKey("application/json"))
+            {
+                content.Add("application/json", new OpenApiMediaType());
+            }
+        }
+    }
+}
